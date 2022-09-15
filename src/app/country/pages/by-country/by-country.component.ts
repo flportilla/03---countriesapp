e541b4db116cbaa0de98b4f63cba707dbe8f40bd
Fabian Portilla <fas_456@hotmail.com>
2022-09-15 16:33:56 -0500

    Add suggestions

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { CountryService } from '../../services/country.service';
 import { CountryResponse } from '../../interfaces/country.interface';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
   styles: [
+    `
+    li{
+      cursor: pointer
+    }
+    `
   ]
 })
 export class ByCountryComponent  {
 
   public term: string = ''
   public isError: boolean = false;
+  public showSuggestions: boolean = false;
   public countries: CountryResponse[] = []
+  public suggestedCountries: CountryResponse[] = []
 
   constructor(private CountryService: CountryService) { }
 
@@ -36,5 +44,18 @@ export class ByCountryComponent  {
 
   suggestions(term: string){
     this.isError = false
+    this.term = term
+    this.showSuggestions = true
+
+    this.CountryService.searchCountry(term)
+      .subscribe({
+        next: (country) => this.suggestedCountries = country.splice(0,5),
+        error: (error) => this.suggestedCountries = []
+      })
+
+  }
+
+  searchSuggestions(term: string){
+    this.search(term)
   }
 }
